076919c1142f32eceb64fb2b36e038d062e6b905
Gaudencia <78945952+Gaudencia-Otara@users.noreply.github.com>
2022-12-18 16:01:56 +0300

    login and signup pages

---
+++ FrontEnd/Authentication/forgotpassword.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>genz</title>
+    <link rel="stylesheet" href="../css/login.css">
+
+    <style>
+@import url('https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400');
+</style>
+<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+
+</head>
+<body>
+    <!-- <h3> <hr> </h3> -->
+    <div class="iconform">
+    <div class="form">
+      <h4>Enter Your Account Email</h4>
+    <form action="" method="POST">
+    <input type="text" placeholder="Email"> <br>
+
+     <button>Send Code</button>
+     <br>
+       </form>
+ 
+</body>
+</html>
---
+++ FrontEnd/Authentication/login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>genz</title>
+    <link rel="stylesheet" href="../css/login.css">
+
+    <style>
+@import url('https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400');
+</style>
+<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+
+</head>
+<body>
+    <!-- <h3> <hr> </h3> -->
+    <div class="iconform">
+    <div class="form">
+      <h4>Login to your account</h4>
+    <form action="" method="POST">
+   <!-- <label for="email">Email</label> <br> -->
+   <input type="text" placeholder="Email"> <br>
+   <!-- <label for="password">Password</label><br> -->
+   <input type="password" placeholder="Password"> <br>
+     <button>Log In</button>
+     <br>
+     <a href="forgotpassword.php"><h5>Forgot password? </h5></a>
+      </form>
+      <div class="lowertext">
+    <p style="font-size: 15px;">-Or Sign In With-</p>
+    </div>
+    <div class="icons">
+    <ul class="icons-center">
+  <li><a href="#"><i class="fab fa-facebook" aria-hidden="true" ></i></a></li>
+  <li><a href="#"><i class="fab fa-twitter" aria-hidden="true"></i></a></li>
+  <li><a href="#"><i class="fab fa-google-plus-g" aria-hidden="true"></i></a></li>
+  </ul>
+    </div>
+   <p>Don't Have An Account? <a href="signup.php"><b>Sign Up</b></a></p>
+    </div>
+    </div>
+</body>
+</html>
